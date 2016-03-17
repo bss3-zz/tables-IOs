1aab6534041c07b64edad12c7cbc0e40b62a5460
Bruno Soares da Silva <bss3@cin.ufpe.br>
2016-03-17 12:02:09 -0300

    Initial Commit

---
+++ tables/TodoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodoTableViewCell.swift
+//  tables
+//
+//  Created by Bruno Soares da Silva on 3/16/16.
+//  Copyright © 2016 Bruno Soares da Silva. All rights reserved.
+//
+
+import UIKit
+
+class TodoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
